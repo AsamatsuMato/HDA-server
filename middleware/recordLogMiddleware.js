ce2abdb399fc8445d3744bdb9372656799ad2699
Asamatsu_Mato <571957800@qq.com>
2024-04-03 12:00:34 +0800

    [优化]（日志记录）新增日志记录中间件

--- middleware/recordLogMiddleware.js
+++ middleware/recordLogMiddleware.js
@@ -4,10 +4,11 @@ module.exports = (req, res, next) => {
   const path = require("path");
 
   const date = monent().format("YYYY-MM-DD HH:mm:ss");
-  const { url, ip } = req;
+  const { url, ip, method } = req;
+  const { statusCode } = res;
   fs.appendFileSync(
     path.resolve(__dirname + "/../log/interview.log"),
-    `${date}\t\t${url}\t\t${ip}\r\n`
+    `${date}\t\t${method}\t\t${statusCode}\t\t${url}\t\t${ip}\r\n`
   );
   next();
 };
